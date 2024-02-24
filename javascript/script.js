9df6e17e822fa8041170c718f11ea93a7a83496f
Sayad Hakeem H A <sayadhakeemha@gmail.com>
2024-02-24 22:15:34 +0530

    h

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,14 @@ bars.addEventListener("click",()=>{
     bars.classList.toggle("fa-times");
     header.classList.toggle("toggle");
 })
+
+// Autotyping
+
+const exampleText = ['React Developer', 'Js Developer', 'Front-end Developer'];
+const exampleTyping = new AutoTyping('.example-selector', exampleText, {
+  typeSpeed: 50,
+  deleteSpeed: 50,
+  waitBeforeDelete: 2000,
+  waitBetweenWords: 500,
+});
+exampleTyping.start()
